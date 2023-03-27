838126970389a813b8ff87d5f02838fa5d7d95e3
BenjaminOrtizJr <bortizjr84@outlook.com>
2023-03-27 00:32:00 -0400

    #

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { VerticalTimeline, VerticallTimelineElement } from 'react-vertical-timeline-componenent';
+import "react-vertical-timeline-component/style.min.css"
+import { motion } from 'framer-motion'
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2></h2>
+      </motion.div>
+    </>
   )
 }
 
